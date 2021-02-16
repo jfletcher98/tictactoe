b7b7db16a859747880af0d3f7b6405f482617f62
jfletcher98 <jofl3978@colorado.edu>
2021-02-15 20:07:56 -0700

    Implemented marker placement for players

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -7,6 +7,7 @@ class Board{
     public:
         void CreateBoard();
         void DisplayBoard();
+        void PlaceMarker(int row, int col, int playerNum);
 
     private:
         SquareType arr_[3][3];
@@ -26,11 +27,11 @@ void Board::DisplayBoard() {
     for(int i = 0; i < 3; i++) {
         for(int j = 0; j < 3; j++) {
             if(arr_[i][j] == SquareType::Empty){
-                std::cout<<"⚪ ";;
+                std::cout<<" ⚪ ";;
             } else if(arr_[i][j] == SquareType::Player1) {
-                std::cout<<"X ";;
+                std::cout<<" ❌ ";;
             } else {
-                std::cout<<"O ";;
+                std::cout<<" ⭕ ";;
             }
         }
         std::cout<<std::endl;
@@ -39,9 +40,24 @@ void Board::DisplayBoard() {
     std::cout<<"---------------------------"<<std::endl;
 }
 
+void Board::PlaceMarker(int row, int col, int playerNum) {
+    if(playerNum == 1) {
+        arr_[row][col] = SquareType::Player1;
+    }
+    else if(playerNum == 2) {
+        arr_[row][col] = SquareType::Player2;
+    }
+}
+
 int main() {
     Board b1;
+
     b1.CreateBoard();
     b1.DisplayBoard();
+    b1.PlaceMarker(1, 1, 1);
+    b1.DisplayBoard();
+    b1.PlaceMarker(2, 2, 2);
+    b1.DisplayBoard();
+
     return 0;
 }
